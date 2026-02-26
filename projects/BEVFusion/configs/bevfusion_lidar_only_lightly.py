53906b5c983970f305d8136a8ae7b126da7aa79d
Noah Oldmann <yichongyangbit@163.com>
2026-02-26 21:25:34 +0800

    [20260226 MOD: 尝试缩小点云空间108m到30m]

--- projects/BEVFusion/configs/bevfusion_lidar_only_lightly.py
+++ projects/BEVFusion/configs/bevfusion_lidar_only_lightly.py
@@ -8,7 +8,9 @@ custom_imports = dict(
 # If point cloud range is modified, do remember to change all related
 # keys in the config.
 voxel_size = [0.075, 0.075, 0.2]
-point_cloud_range = [-54.0, -54.0, -5.0, 54.0, 54.0, 3.0]
+point_cloud_range = [-15.0, -15.0, -5.0, 15.0, 15.0, 3.0]
+sparse_shape = [400, 400, 41]
+grid_size = [400, 400, 41]
 class_names = [
     'car', 'truck', 'construction_vehicle', 'bus', 'trailer', 'barrier',
     'motorcycle', 'bicycle', 'pedestrian', 'traffic_cone'
@@ -48,15 +50,15 @@ model = dict(
         pad_size_divisor=32,
         voxelize_cfg=dict(
             max_num_points=10,
-            point_cloud_range=[-54.0, -54.0, -5.0, 54.0, 54.0, 3.0],
-            voxel_size=[0.075, 0.075, 0.2],
+            point_cloud_range=point_cloud_range,
+            voxel_size=voxel_size,
             max_voxels=[120000, 160000],
             voxelize_reduce=True)),
     pts_voxel_encoder=dict(type='HardSimpleVFE', num_features=5),
     pts_middle_encoder=dict(
         type='BEVFusionSparseEncoder',
         in_channels=5,
-        sparse_shape=[1440, 1440, 41],
+        sparse_shape=sparse_shape,
         order=('conv', 'norm', 'act'),
         norm_cfg=dict(type='BN1d', eps=0.001, momentum=0.01),
         encoder_channels=((16, 16, 32), (32, 32, 64), (64, 64, 128), (128,
@@ -104,9 +106,9 @@ model = dict(
             pos_encoding_cfg=dict(input_channel=2, num_pos_feats=128)),
         train_cfg=dict(
             dataset='nuScenes',
-            point_cloud_range=[-54.0, -54.0, -5.0, 54.0, 54.0, 3.0],
-            grid_size=[1440, 1440, 41],
-            voxel_size=[0.075, 0.075, 0.2],
+            point_cloud_range=point_cloud_range,
+            grid_size=grid_size,
+            voxel_size=voxel_size,
             out_size_factor=8,
             gaussian_overlap=0.1,
             min_radius=2,
@@ -124,20 +126,20 @@ model = dict(
                 iou_cost=dict(type='IoU3DCost', weight=0.25))),
         test_cfg=dict(
             dataset='nuScenes',
-            grid_size=[1440, 1440, 41],
+            grid_size=grid_size,
             out_size_factor=8,
-            voxel_size=[0.075, 0.075],
-            pc_range=[-54.0, -54.0],
+            voxel_size=voxel_size[:2],
+            pc_range=point_cloud_range[:2],
             nms_type=None),
         common_heads=dict(
             center=[2, 2], height=[1, 2], dim=[3, 2], rot=[2, 2], vel=[2, 2]),
         bbox_coder=dict(
             type='TransFusionBBoxCoder',
-            pc_range=[-54.0, -54.0],
+            pc_range=point_cloud_range[:2],
             post_center_range=[-61.2, -61.2, -10.0, 61.2, 61.2, 10.0],
             score_threshold=0.0,
             out_size_factor=8,
-            voxel_size=[0.075, 0.075],
+            voxel_size=voxel_size[:2],
             code_size=10),
         loss_cls=dict(
             type='mmdet.FocalLoss',
@@ -255,7 +257,7 @@ test_pipeline = [
         backend_args=backend_args),
     dict(
         type='PointsRangeFilter',
-        point_cloud_range=[-54.0, -54.0, -5.0, 54.0, 54.0, 3.0]),
+        point_cloud_range=point_cloud_range),
     dict(
         type='Pack3DDetInputs',
         keys=['img', 'points', 'gt_bboxes_3d', 'gt_labels_3d'],
